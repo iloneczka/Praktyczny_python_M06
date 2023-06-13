016a6a3cbbe5e4a67edef7355f2ce9bd91b32073
Ilona Pokrywka <ilo@MacBook-Air-Ilona.local>
2023-06-13 15:28:57 +0200

    Pierwsza działająca wersja projektu.

--- Projekt 6.py
+++ Projekt 6.py
@@ -1,28 +1,47 @@
-# Napisz własną analitykę do aplikacji do śledzenia czasu takich jak np. Toggl Track. Twój moduł wczytuje dane o czasie wykonania poszczególnych zadań w formacie CSV, a następnie generuje raport wykorzystania czasu z podziałem na projekty, klientów i inne tagi.
+"""
+Module for generating a time utilization report based on data from a CSV file.
 
-# 1. Dane w pliku CSV mają trzy kolumny: desc, time oraz tags. Desc jest opisem zadania. Time to liczba całkowita określająca czas wykonywania tego zadania w minutach. Natomiast tags jest listą tagów porozdzielanych spacjami. Jedno zadanie może mieć wiele tagów. Jednym tagiem może być otagowane wiele zadań. Tagi służą do oznaczania zadań wg projektów, klientów lub innych kryteriów.
+The CSV file should have three columns: 'desc', 'time', and 'tags'. 'desc' represents the task description,
+'time' is the task duration in minutes (an integer), and 'tags' is a space-separated list of tags.
+Tags are used to categorize tasks by projects, clients, or other criteria.
 
-# 2. Upakuj te trzy informacje w klasie Entry. Upewnij się, że ma ona metodę __repr__.
+The program accepts the path to the CSV file as a command-line argument and displays a report.
+Each line of the report corresponds to a tag, and it shows the total time spent on tasks with that tag.
 
-# 3. Program przyjmuje scieżkę do pliku CSV jako argument linii poleceń. Następnie wyświetla raport. Każda jego linia to jeden tag. Dla każdego tagu wyliczona jest suma wszystkich otagowanych nim zadań.
+"""
+# python3 "Praktyczny_python_M06/Projekt 6.py" "Praktyczny_Python/M06/track.csv"
 
-# 4. Nie musisz pisać testów, ale podziel program na funkcje tak, aby każda z nich robiła tylko jedną rzecz.
-
+import click
 import csv
+from typing import Dict, List
 
 class Entry:
-    def __init__(self, desc, time, tags):
+    """
+    Class representing a task entry.
+
+    Attributes:
+        desc (str): The description of the task.
+        time (int): The duration of the task in minutes.
+        tags (List[str]): A list of tags associated with the task.
+    """
+    def __init__(self, desc: str, time: int, tags: List[str]):
         self.desc = desc
         self.time = time
         self.tags= tags
 
-    def __repr__(self):
-       return f"POMIDOR(desc='{self.desc!r}', time={self.time!r}, tags={self.tags!r})"
-    
-    def __str__(self):
-        return self.__repr__()
+    def __repr__(self) -> str:
+       return f"(desc='{self.desc!r}', time={self.time!r}, tags={self.tags!r})"
 
-def load_entries_from_csv(filename):
+def load_entries_from_csv(filename: str) -> List[Entry]:
+    """
+    Load task entries from a CSV file and return a list of Entry objects.
+
+    Args:
+        filename: Path to the CSV file.
+
+    Returns:
+        A list of Entry objects representing the task entries.
+    """
     list_of_entries= []
     with open(filename) as stream:
         reader = csv.DictReader(stream)  
@@ -36,18 +55,51 @@ def load_entries_from_csv(filename):
         # print(list_of_entries)
         return list_of_entries
 
-# def edit_by_tags(list_of_entries):
-#     tags_= {}
-#     for entry in list_of_entries:
+def count_time_by_tag(list_of_entries: List[Entry]) -> Dict[str, int]:
+    """
+    Count the total time for each tag in the given list of entries.
 
+    Args:
+        list_of_entries: A list of Entry objects representing the task entries.
 
-def main():
-    FILENAME= "/Users/ilo/Desktop/PYTHON/Praktyczny_Python/M06/track.csv"
-    list= load_entries_from_csv(FILENAME)
-    print(list)
+    Returns:
+        A dictionary where the keys are tags and the values are the total time spent on tasks with that tag.
+    """
+    time_counter = {}
+    for entry in list_of_entries:
+        tags = entry.tags  
+        time = entry.time
+        for tag in tags:
+            if tag in time_counter:
+                time_counter[tag] += time
+            else:
+                time_counter[tag] = time
+    return time_counter
+    
+def display_tag_and_time(time_counter: Dict[str, int]) -> None:
+    """
+    Display the tag and corresponding total time in a formatted table.
 
+    Args:
+        time_counter: A dictionary where the keys are tags and the values are the total time spent on tasks with that tag.
+    """
+    print(f"{'TOTAL TIME':>12s}  {'TAG'}")
+    print("-------------------------")
+    for tag,time in time_counter.items():
+        print(f"{str(time):>12s}  #{tag}")
 
+@click.command()
+@click.argument('csv_file')    
+def main(csv_file: str) -> None:
+    """
+    Main entry point of the program.
 
-
+    Args:
+        csv_file: Path to the CSV file containing task entries.
+    """
+    list= load_entries_from_csv(csv_file)
+    list_with_counter_and_time= count_time_by_tag(list)
+    display_tag_and_time(list_with_counter_and_time)
+  
 if __name__ == '__main__':
     main()
